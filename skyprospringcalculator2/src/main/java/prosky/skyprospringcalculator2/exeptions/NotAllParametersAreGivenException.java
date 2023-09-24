517a1c6e498646ca3f78efd7779e5aa30a16f528
Artnes <2023java@mail.ru>
2023-09-24 18:12:19 +0300

    HW 2.12 - готовое

---
+++ skyprospringcalculator2/src/main/java/prosky/skyprospringcalculator2/exeptions/NotAllParametersAreGivenException.java
@@ -0,0 +1,8 @@
+package prosky.skyprospringcalculator2.exceptions;
+
+public class NotAllParametersAreGivenException extends RuntimeException {
+    public NotAllParametersAreGivenException(String message) {
+        super(message);
+    }
+}
+
