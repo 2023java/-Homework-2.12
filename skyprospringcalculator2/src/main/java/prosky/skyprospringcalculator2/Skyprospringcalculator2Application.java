861b23e2bf0753306bf4813be7b2306c2eb6492e
Artnes <2023java@mail.ru>
2023-09-24 17:03:15 +0300

    HW 2.12 - начало

---
+++ skyprospringcalculator2/src/main/java/prosky/skyprospringcalculator2/Skyprospringcalculator2Application.java
@@ -0,0 +1,13 @@
+package prosky.skyprospringcalculator2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skyprospringcalculator2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skyprospringcalculator2Application.class, args);
+	}
+
+}
---
+++ skyprospringcalculator2/src/test/java/prosky/skyprospringcalculator2/Skyprospringcalculator2ApplicationTests.java
@@ -0,0 +1,13 @@
+package prosky.skyprospringcalculator2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skyprospringcalculator2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
